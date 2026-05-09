b9d95300fe5b9e3162833ff0d4e6412d6adf066d
Manish <262701884+buildwith-manish@users.noreply.github.com>
2026-05-09 07:04:31 +0000

    Fix CallLogService stub with all required methods

--- family_monitor/lib/services/call_log_service.dart
+++ family_monitor/lib/services/call_log_service.dart
@@ -1,6 +1,22 @@
+class CallRecord {
+  final String number;
+  final String name;
+  final String type;
+  final DateTime date;
+  final int duration;
+  CallRecord({
+    required this.number,
+    required this.name,
+    required this.type,
+    required this.date,
+    required this.duration,
+  });
+}
+
 class CallLogService {
-  // Call log feature disabled - requires special Android permissions
-  Future<List<Map<String, dynamic>>> getCallLogs(String childUid) async {
-    return [];
-  }
+  Future<List<CallRecord>> getCallLogs(String childUid) async => [];
+  Stream<List<CallRecord>> watchCallLog(String childUid) => Stream.value([]);
+  Future<void> syncCallLog() async {}
+  Future<void> requestSync(String childUid) async {}
+  Stream<bool> watchSyncRequest(String uid) => Stream.value(false);
 }
